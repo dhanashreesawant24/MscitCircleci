e537fe4f3e117abf174f1fd898e7e37107833057
dhanashreesawant24 <dhanashree@sparkplustech.com>
2023-06-11 11:38:04 +0530

    multiplication and division added

--- calculator.py
+++ calculator.py
@@ -6,8 +6,20 @@ def myAdd(num1,num2):
 def mySub(num1,num2):
     return num1-num2
 
+#Multiplication Operation
+def myMul(num1,num2):
+    return num1*num2
+
+#Division Operation
+def myDiv(num1,num2):
+    return num1/num2
+
 result1=myAdd(20,30)
 result2=mySub(60,20)
+result3=myMul(50,50)
+result4=myDiv(10,2)
 
 print(result1)
 print(result2)
+print(result3)
+print(result4)
