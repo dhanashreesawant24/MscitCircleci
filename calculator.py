750c12c0c86ead8284740d82d672edc710bc12ca
dhanashreesawant24 <dhanashree@sparkplustech.com>
2023-06-11 11:33:00 +0530

    added substraction code

--- calculator.py
+++ calculator.py
@@ -1,4 +1,13 @@
+#Addition Operation
 def myAdd(num1,num2):
     return num1+num2
-result=myAdd(20,30)
-print(result)
+
+#Substraction Operation
+def mySub(num1,num2):
+    return num1-num2
+
+result1=myAdd(20,30)
+result2=mySub(60,20)
+
+print(result1)
+print(result2)
